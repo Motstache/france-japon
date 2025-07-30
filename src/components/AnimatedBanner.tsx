d66edc1865027f6be0580242031a3b1429f5c8ba
Gauthier <you@example.comgauthier.nagel@gmail.com>
2025-07-30 20:53:48 +0200

    Ajout bannière animée avec fond défilant et motos fixes

--- src/components/AnimatedBanner.tsx
+++ src/components/AnimatedBanner.tsx
@@ -2,20 +2,31 @@ import React from "react";
 
 const AnimatedBanner: React.FC = () => {
   return (
-    <div className="relative w-full overflow-hidden h-32 mt-8">
+    <div className="relative w-full overflow-hidden h-32 mt-8 flex items-end justify-center">
+      {/* Fond panoramique qui défile */}
       <div
         className="absolute inset-0 animate-slide"
         style={{
-          backgroundImage: "url('/banner-motos.png')",
+          backgroundImage: "url('/banner-paysage.png')", // fond panoramique
           backgroundSize: "contain",
           backgroundRepeat: "repeat-x",
-          backgroundPosition: "center",
+          backgroundPosition: "bottom",
         }}
       />
+
+      {/* Motos fixes au premier plan */}
+      <div className="relative z-10">
+        <img
+          src="/moto-silhouettes.png"
+          alt="Motos"
+          className="h-24 object-contain"
+        />
+      </div>
+
       <style>{`
         @keyframes slide {
-          0% { background-position: 0 0; }
-          100% { background-position: -2000px 0; }
+          0% { background-position: 0 bottom; }
+          100% { background-position: -2000px bottom; }
         }
         .animate-slide {
           animation: slide 25s linear infinite;
