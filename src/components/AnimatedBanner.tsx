ed30437571a0ce809ebd9837badf2aa855002064
Gauthier <you@example.comgauthier.nagel@gmail.com>
2025-07-30 22:55:36 +0200

    Revert: retour au code initial de la bannière animée

--- src/components/AnimatedBanner.tsx
+++ src/components/AnimatedBanner.tsx
@@ -2,28 +2,23 @@ import React from "react";
 
 const AnimatedBanner: React.FC = () => {
   return (
-    <div className="relative w-screen overflow-hidden aspect-[2/1] max-h-32 mt-8">
+    <div className="relative w-full overflow-hidden h-32 mt-8">
       {/* Fond panoramique qui défile */}
       <div
         className="absolute inset-0 animate-slide"
         style={{
           backgroundImage: "url('/banner-paysage.png')",
-          backgroundSize: "cover",
+          backgroundSize: "contain",
           backgroundRepeat: "repeat-x",
-          backgroundPosition: "bottom center",
+          backgroundPosition: "bottom",
         }}
       />
 
-      {/* Motos fixes centrées */}
+      {/* Motos fixes, légèrement descendues */}
       <img
         src="/moto-silhouettes.png"
         alt="Motos"
-        className="
-          absolute bottom-0 left-1/2 -translate-x-1/2
-          translate-y-2 sm:translate-y-3 md:translate-y-4 lg:translate-y-5
-          h-[30%] sm:h-[35%] md:h-[40%] lg:h-[45%]
-          object-contain z-10
-        "
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-2 h-24 object-contain z-10"
       />
 
       <style>{`
