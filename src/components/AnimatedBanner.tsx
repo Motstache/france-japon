4c90944ce91857bd98adeed0b5824915625b00c2
Gauthier <you@example.comgauthier.nagel@gmail.com>
2025-07-30 22:50:58 +0200

    Fix: la bannière occupe toute la largeur de l'écran (w-screen)

--- src/components/AnimatedBanner.tsx
+++ src/components/AnimatedBanner.tsx
@@ -2,26 +2,26 @@ import React from "react";
 
 const AnimatedBanner: React.FC = () => {
   return (
-    <div className="relative w-full overflow-hidden h-32 mt-8">
+    <div className="relative w-screen overflow-hidden h-32 mt-8">
       {/* Fond panoramique qui défile */}
       <div
         className="absolute inset-0 animate-slide"
         style={{
           backgroundImage: "url('/banner-paysage.png')",
-          backgroundSize: "contain",
+          backgroundSize: "cover", // couvre toute la largeur
           backgroundRepeat: "repeat-x",
           backgroundPosition: "bottom",
         }}
       />
 
-      {/* Motos fixes, position adaptée selon la taille d'écran */}
+      {/* Motos fixes centrées */}
       <img
         src="/moto-silhouettes.png"
         alt="Motos"
         className="
-          absolute bottom-0 left-1/2 -translate-x-1/2 
-          translate-y-2 sm:translate-y-3 md:translate-y-4 lg:translate-y-5 
-          h-20 sm:h-24 md:h-28 lg:h-32 
+          absolute bottom-0 left-1/2 -translate-x-1/2
+          translate-y-2 sm:translate-y-3 md:translate-y-4 lg:translate-y-5
+          h-20 sm:h-24 md:h-28 lg:h-32
           object-contain z-10
         "
       />
